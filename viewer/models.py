2c245be24bf5b3662572f69de7b72a90e2f55f8b
bastantoine <bastien.antoine69+github@gmail.com>
2020-08-15 17:04:35 +0200

    Added Album and Picture models

--- viewer/models.py
+++ viewer/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Album(models.Model):
+    name = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    parent_album = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
+
+
+class Picture(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    path = models.ImageField()
