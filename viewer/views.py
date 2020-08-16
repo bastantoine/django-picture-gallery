00d737b1730b19c7d76a48aee114d3f99005b6b5
bastantoine <bastien.antoine69+github@gmail.com>
2020-08-16 15:16:16 +0200

    Added AlbumView in the Viewer to display the pictures of an album

--- viewer/views.py
+++ viewer/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.views import View
 
-from .models import Album
+from .models import Album, Picture
 
 
 class HomeView(View):
@@ -11,3 +11,15 @@ class HomeView(View):
             'albums': Album.objects.all(),
         }
         return render(request, 'viewer/home.html', context=context)
+
+
+class AlbumView(View):
+
+    def get(self, request, id_album):
+        album = get_object_or_404(Album, pk=id_album)
+        list_pictures = get_list_or_404(Picture, album__id=id_album)
+        context = {
+            'album': album,
+            'list_pictures': list_pictures,
+        }
+        return render(request, 'viewer/album.html', context=context)
