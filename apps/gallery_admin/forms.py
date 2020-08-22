59237e11dd91d857d5f4ab761822ad3c5c9dfcae
bastantoine <bastien.antoine69+github@gmail.com>
2020-08-22 21:17:59 +0200

    Better form with the crispy

--- apps/gallery_admin/forms.py
+++ apps/gallery_admin/forms.py
@@ -1,5 +1,8 @@
 from django.forms import ModelForm
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
 from apps.core.models import Album
 
 
@@ -7,3 +10,13 @@ class AlbumForm(ModelForm):
     class Meta:
         model = Album
         fields = ['name', 'start_date', 'end_date', 'parent_album', 'description', 'is_protected']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_class = 'form-horizontal'
+        self.helper.form_method = 'post'
+        self.helper.form_action = 'gallery_admin:add_album'
+        self.helper.label_class = 'col-lg-2'
+        self.helper.field_class = "col-lg-10"
+        self.helper.add_input(Submit('submit', 'Add album'))
