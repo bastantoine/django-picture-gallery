4b1dc26dc9351a53f0b83ec12fa3d80cc6410d64
bastantoine <bastien.antoine69+github@gmail.com>
2020-09-01 17:44:02 +0200

    Make sure PG is up and running before starting Django

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
 # From https://stackoverflow.com/questions/33992867/how-do-you-perform-django-database-migrations-when-using-docker-compose
 
 # Apply database migrations
