1df010f01c480a544614b983cbf9d7414eedc52d
bastantoine <bastien.antoine69+github@gmail.com>
2020-09-01 17:47:29 +0200

    django makemigrations runs on all apps

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -17,10 +17,6 @@ fi
 echo "Collect database migrations"
 python manage.py makemigrations
 
-# Apply database migrations for the viewer
-echo "Apply database migrations for the viewer"
-python manage.py makemigrations viewer
-
 # Apply database migrations
 echo "Apply database migrations"
 python manage.py migrate
